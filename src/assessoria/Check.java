148221fc9b0d639b00dd8168fecb4d0d9b3d452a
Murgalha <murilobaldi1@gmail.com>
2018-06-22 14:35:35 -0300

    Add comments

--- src/assessoria/Check.java
+++ src/assessoria/Check.java
@@ -35,6 +35,7 @@ public class Check {
         
         String check = input.get(n-1);
 
+        /* Search table 'FESTA' looking for primary key entered */
         try {
             st2 = db.getConnection().createStatement();
             rs2 = st2.executeQuery("SELECT NOTA_FISCAL, TIPO FROM FESTA WHERE NOTA_FISCAL='"+check+"'");
@@ -42,9 +43,11 @@ public class Check {
         catch(Exception e) {}
 
         switch(tableName) {
+            /* case 'FESTA INFANTIL' */
         case "CONTRATO_ANIMADOR":
         case "CONTRATO_BRINQUEDO":
             try {
+                /* Check if constraints are right */
                 rs2.next();
                 if(rs2.getString("NOTA_FISCAL").equals(check) &&
                    rs2.getString("TIPO").equals("INFANTIL"))
@@ -62,10 +65,12 @@ public class Check {
                 }
             }
             catch(Exception e) {}
+        /* CASE 'CASAMENTO' */
         case "CONTRATO_SOM_LUZ":
         case "CONTRATO_CERIMONIALISTA":
         case "CONTRATO_BANDA":
             try {
+                /* Check if constraints are right */
                 rs2.next();
                 if(rs2.getString("NOTA_FISCAL").equals(check) &&
                    rs2.getString("TIPO").equals("CASAMENTO"))
@@ -83,6 +88,7 @@ public class Check {
                 return false;
             }
             catch (Exception e) {}
+        /* case other type of table, do not check */
         default:
             return true;
         }
@@ -93,25 +99,29 @@ public class Check {
         ResultSet rs1 = null, rs2 = null;
         ResultSetMetaData rsmd = null;
         int nCols = 0, n = -1;
-        
+
+        /* Get table info */
         try {
             st1 = db.getConnection().createStatement();
             rs1 = st1.executeQuery("SELECT * FROM " + tableName);            
+            /* Get meta data and number of columns */
             rsmd = rs1.getMetaData();
             nCols = rsmd.getColumnCount();
         }
         catch (Exception e) {}
 
+        /* Try to find a column named 'LOCAL' */
         try {            
             n = rs1.findColumn("LOCAL");
         }
         catch (Exception e) {}
-
+        
+        /* If there are none, there is no reason to continue the check */
         if(n == -1)
             return true;
-        
+        /* Get 'LOCAL' input from array */
         String check = input.get(n-1);
-
+        
         try {
             st2 = db.getConnection().createStatement();
             rs2 = st2.executeQuery("SELECT ID, TIPO FROM LOCAL WHERE ID='"+check+"'");
@@ -119,8 +129,10 @@ public class Check {
         catch(Exception e) {}
 
         switch(tableName) {
+            /* If insert is into 'BUFE_INFANTIL' */
         case "BUFE_INFANTIL":
             try {
+                /* Check if constraints are right */
                 rs2.next();
                 if(rs2.getString("ID").equals(check) &&
                    rs2.getString("TIPO").equals("BUFE INFANTIL"))
@@ -138,8 +150,10 @@ public class Check {
                 }
             }
             catch(Exception e) {}
+            /* If insert is into 'DECORACAO_SALAO' */
         case "DECORACAO_SALAO":
             try {
+                /* Check if constraints are right */
                 rs2.next();
                 if(rs2.getString("ID").equals(check) &&
                    rs2.getString("TIPO").equals("BUFE INFANTIL"))
@@ -157,6 +171,7 @@ public class Check {
                 return false;
             }
             catch (Exception e) {}
+            /* Do not check if table is not one of the above */
         default:
             return true;
         }
@@ -167,7 +182,8 @@ public class Check {
         ResultSet rs1 = null, rs2 = null;
         ResultSetMetaData rsmd = null;
         int nCols = 0, n = -1, m = -1;
-        
+
+        /* Get table meta data */
         try {
             st1 = db.getConnection().createStatement();
             rs1 = st1.executeQuery("SELECT * FROM " + tableName);            
@@ -176,6 +192,7 @@ public class Check {
         }
         catch (Exception e) {}
 
+        /* Try to find column named 'LOCAL' */
         try {            
             n = rs1.findColumn("LOCAL");
         }
@@ -186,6 +203,7 @@ public class Check {
         
         String checkLocal = input.get(n-1);
 
+        /* Try to find column named 'TIPO' */
         try {            
             m = rs1.findColumn("TIPO");
         }
@@ -196,6 +214,7 @@ public class Check {
         
         String checkFesta = input.get(m-1);
 
+        /* Get constraints info from tables */
         try {
             st2 = db.getConnection().createStatement();
             rs2 = st2.executeQuery("SELECT L.TIPO AS TIPO_LOCAL FROM FESTA F JOIN LOCAL L ON L.ID="+checkLocal);
@@ -205,6 +224,7 @@ public class Check {
         switch(tableName) {
         case "FESTA":
             try {
+                /* Check constraints */
                 rs2.next();
                 if(rs2.getString("TIPO_LOCAL").equals("BUFE INFANTIL") &&
                    checkFesta.equals("INFANTIL"))
@@ -228,6 +248,7 @@ public class Check {
                 }
             }
             catch(Exception e) {}
+            /* Do not check if table is not 'FESTA' */
         default:
             return true;
         }
